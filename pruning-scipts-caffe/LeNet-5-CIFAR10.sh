b837abb30a8b58bbbf74fc8c67b1d41a3a8dcc88
kaveena <persandk@scss.tcd.ie>
2020-05-11 17:08:24 +0100

    homogenise prototxt

--- pruning-scipts-caffe/LeNet-5-CIFAR10.sh
+++ pruning-scipts-caffe/LeNet-5-CIFAR10.sh
@@ -6,5 +6,5 @@ eval_size=2
 test_size=80
 test_interval=1
 tolerance=10.0
-iterations=2
+iterations=8
 . ./pruning-scipts-caffe/base_info.sh
--- pruning-scipts-caffe/NIN-CIFAR10.sh
+++ pruning-scipts-caffe/NIN-CIFAR10.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-arch=NIN-CIFAR10
+arch=NIN
+dataset=CIFAR10
 train_size=25
-eval_size=10
-test_size=10
-test_interval=1
+eval_size=2
+test_size=80
+test_interval=10
 tolerance=10.0
-
+iterations=8
 . ./pruning-scipts-caffe/base_info.sh
