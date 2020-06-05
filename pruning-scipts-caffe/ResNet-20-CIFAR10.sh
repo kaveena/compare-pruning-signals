2080105ea7d7c285a1455d1570b92d8746a204d2
kaveena <persandk@tcd.ie>
2020-06-05 11:11:23 +0100

    retraining hyperparameters for ResNet-20

--- pruning-scipts-caffe/ResNet-20-CIFAR10.sh
+++ pruning-scipts-caffe/ResNet-20-CIFAR10.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 arch=ResNet-20
 dataset=CIFAR10
-train_size=256
+train_size=25
 eval_size=2
 test_size=80
 test_interval=1
 tolerance=10.0
-iterations=8
-stop_acc=60.0
+iterations=2
+stop_acc=80.0
 . ./pruning-scipts-caffe/base_info.sh
--- pruning-scipts-caffe/ResNet-20-CIFAR100.sh
+++ pruning-scipts-caffe/ResNet-20-CIFAR100.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 arch=ResNet-20
 dataset=CIFAR100
-train_size=256
+train_size=25
 eval_size=2
 test_size=80
 test_interval=1
 tolerance=10.0
 iterations=8
-stop_acc=40.0
+stop_acc=50.0
 . ./pruning-scipts-caffe/base_info.sh
