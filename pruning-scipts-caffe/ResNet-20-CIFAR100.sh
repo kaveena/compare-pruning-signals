ce5b961db9554b735fad64535b252835568c1d4a
kaveena <persandk@tcd.ie>
2020-07-15 22:31:35 +0100

    stop acc ResNet 20

--- pruning-scipts-caffe/ResNet-20-CIFAR100.sh
+++ pruning-scipts-caffe/ResNet-20-CIFAR100.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 arch=ResNet-20
 dataset=CIFAR100
-train_size=25
+train_size=50
 eval_size=2
 test_size=80
 test_interval=1
 tolerance=10.0
 iterations=8
-stop_acc=50.0
+stop_acc=45.0
 . ./pruning-scipts-caffe/base_info.sh
