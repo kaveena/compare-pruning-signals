472b328b2325a345eff6618b3428c48961ae764f
kaveena <persandk@tcd.ie>
2021-06-17 18:22:24 +0000

    add pruning scripts for nfnet and resnet on imagenet

---
+++ pruning-scipts-caffe/ResNet-20-IMAGENET2012.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+arch=ResNet-20
+dataset=IMAGENET2012
+train_size=10
+eval_size=8
+test_size=400
+test_interval=1
+tolerance=10.0
+iterations=4
+stop_acc=50
+. ./pruning-scipts-caffe/base_info.sh
+#. ./pruning-scipts-caffe/base_var_gradient.sh
+#. ./pruning-scipts-caffe/base_info_python.sh
+#source pruning-scipts-caffe/best_signals.sh
+
+## best weights
+#prune WEIGHT AVERAGE_INPUT L2 weights_removed
+#prune WEIGHT AVERAGE_INPUT L1 l1_normalisation
+## best activations
+#prune ACTIVATION AVERAGE_INPUT L1 l1_normalisation
+#prune ACTIVATION AVERAGE_INPUT ABS_SUM l1_normalisation
+## best gradients
+#prune ACTIVATION AVERAGE_GRADIENT SQR_SUM weights_removed
+#prune ACTIVATION AVERAGE_GRADIENT ABS_SUM no_normalisation
+#
+##overall best weights
+#prune WEIGHT AVERAGE_INPUT L1 l1_normalisation
+#prune WEIGHT AVERAGE_INPUT L2 weights_removed
+#prune WEIGHT AVERAGE_INPUT L1 weights_removed
+#prune WEIGHT AVERAGE_INPUT L1 l2_normalisation
+#prune WEIGHT AVERAGE_INPUT L2 l1_normalisation
+##overall best activations
+#prune ACTIVATION AVERAGE_INPUT L2 weights_removed
+#prune ACTIVATION L1 l1_normalisation
+##overall best gradients
+#prune ACTIVATION TAYLOR_2ND_APPROX2 ABS_SUM weights_removed
+#prune ACTIVATION TAYLOR_2ND_APPROX2 ABS_SUM no_normalisation
+#prune ACTIVATION TAYLOR ABS_SUM weights_removed
+#prune ACTIVATION TAYLOR_2ND_APPROX1 ABS_SUM weights_removed
+#prune ACTIVATION TAYLOR_2ND_APPROX1 ABS_SUM no_normalisation
